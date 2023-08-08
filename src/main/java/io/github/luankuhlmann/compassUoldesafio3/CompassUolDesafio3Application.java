4ea9e702f880d5c277ddd128e812dce793c6e54e
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2023-08-08 13:09:51 -0300

    first commit

---
+++ src/main/java/io/github/luankuhlmann/compassUoldesafio3/CompassUolDesafio3Application.java
@@ -0,0 +1,13 @@
+package io.github.luankuhlmann.compassUoldesafio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassUolDesafio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassUolDesafio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/luankuhlmann/compassUoldesafio3/CompassUolDesafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luankuhlmann.compassUoldesafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassUolDesafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
